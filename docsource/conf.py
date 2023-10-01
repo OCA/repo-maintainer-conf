45d8a630556c8cfe8d981b93401facc7709c38c6
Simone Orsi <simahawk@gmail.com>
2023-10-01 10:48:24 +0200

    Init repo

---
+++ docsource/conf.py
@@ -0,0 +1,19 @@
+extensions = ["sphinx.ext.autodoc"]
+templates_path = [".templates"]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+master_doc = "index"
+project = "OCA Team"
+copyright = "2023, Odoo Community Association (OCA)"
+version = "16.0"
+release = "16.0"
+today_fmt = "%B %d, %Y"
+pygments_style = "sphinx"
+html_last_updated_fmt = "%b %d, %Y"
+html_copy_source = False
+html_show_sourcelink = False
+htmlhelp_basename = "OCA Team"
+html_theme = "sphinx_rtd_theme"
---
+++ docsource/index.rst
@@ -0,0 +1,8 @@
+OCA Structure
+=============
+
+.. toctree::
+   :maxdepth: 2
+
+   repos
+   teams
